9f20e70e9050ec040972e66fc9747f707f9cd4e0
CorineTang <tang.corine@gmail.com>
2016-01-26 23:20:58 +0100

    Added hotspot wifi closer marker in geolocalisation

--- js/geolocalisation.js
+++ js/geolocalisation.js
@@ -16,17 +16,19 @@ function locateUser() {
 	var userLat, userLon;
  	geoMapContainer.locate()
  		.on('locationfound', function(e){
- 			userLat = e.latitude;
+ 			var userLat = e.latitude,
  			userLon = e.longitude;
             var marker = L.marker([userLat, userLon]).bindPopup('Your are here :)');
             geoMapContainer.setView([userLat, userLon], 15);
             geoMapContainer.addLayer(marker);
+            var hwCloser = findHotspotCloser(userLat, userLon);
+            console.log('hwCloser', hwCloser)
+            setHotspotWifiCloser(hwCloser);
         })
        .on('locationerror', function(e){
             console.log(e);
             alert("Location access denied.");
         });
-    findHotspotCloser(userLat, userLon);
 };
 
 function findHotspotCloser(locateLat, locateLon) {
@@ -37,11 +39,13 @@ function findHotspotCloser(locateLat, locateLon) {
 			'nom_site': hotspotList[i].fields.nom_site,
 			'addresse': hotspotList[i].fields.adresse,
 			'code_site': hotspotList[i].fields.code_site,
+			'latitude': hotspotList[i].geometry.coordinates[1],
+			'longitude': hotspotList[i].geometry.coordinates[0]
 		};
 		distanceList[i]['distance'] = L.latLng(locateLat, locateLon).distanceTo(L.latLng(hotspotList[i].geometry.coordinates[1], hotspotList[i].geometry.coordinates[0]));
 	}
 	distanceList.sort(sortByDistance);
-	console.log('distanceList', distanceList)
+	return distanceList.slice(0, 5);
 };
 
 function sortByDistance(a, b) {
@@ -49,4 +53,11 @@ function sortByDistance(a, b) {
 	var distanceB = parseInt(b['distance']);
 
 	return distanceA - distanceB;
+};
+
+function setHotspotWifiCloser(hotspotList) {
+	for (var i = 0; i < hotspotList.length; i++) {
+		var marker = L.marker([hotspotList[i].hotspotData.latitude, hotspotList[i].hotspotData.longitude]);
+		geoMapContainer.addLayer(marker);
+	}
 };
